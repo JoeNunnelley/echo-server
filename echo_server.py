845b821c1cbd10f0e3bf0080860b5ba12d8626f0
JoeNunnelley <joe.nunnelley@socrata.com>
2019-12-01 15:58:11 -0800

    adding multi-client capable echo server

--- echo_server.py
+++ echo_server.py
@@ -1,12 +1,17 @@
 """
-A basic echo server
+A basic echo server with multi-client select() processing
 """
 
+import select
 import socket
 import sys
 import traceback
+import queue
 
 
+# pylint: disable=too-many-nested-blocks
+# pylint: disable=too-many-branches
+# pylint: disable=too-many-statements
 def server(log_buffer=sys.stderr):
     """ The server function """
     # set an address for our server
@@ -16,48 +21,83 @@ def server(log_buffer=sys.stderr):
                          socket.SOCK_STREAM,
                          socket.IPPROTO_TCP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.settimeout(2)
 
     # log that we are building a server
     print("making a server on {0}:{1}".format(*address), file=log_buffer)
 
     sock.bind(address)
-    sock.listen(1)
-    # pylint: disable=too-many-nested-blocks
+    sock.listen(5)
+
+    inputs = [sock]
+    outputs = []
+
     try:
         # the outer loop controls the creation of new connection sockets. The
         # server will handle each incoming connection one at a time.
-        while True:
-            print('waiting for a connection', file=log_buffer)
-            conn, addr = sock.accept()
-
+        while inputs:
             try:
-                print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                print('waiting for a connection', file=log_buffer)
+                # only concerned with readers in this scenario
+                readers, _, _ = select.select(inputs,
+                                              outputs,
+                                              inputs)
 
-                # the inner loop will receive messages sent by the client in
-                # buffers.  When a complete message has been received, the
-                # loop will exit
-                while True:
-                    data = conn.recv(data_chunk)
-                    print('received "{0}"'.format(data.decode('utf8')))
-                    conn.sendall(data)
-                    print('sent "{0}"'.format(data.decode('utf8')))
+                for reader in readers:
+                    # add connections to input connection array
+                    if reader is sock:
+                        conn, addr = sock.accept()
+                        print("new connection client: {0}:{1}".format(*addr))
+                        conn.setblocking(0)
+                        inputs.append(conn)
+                    else:
+                        try:
+                            print('connection - {0}:{1}'.format(*addr),
+                                  file=log_buffer)
+                            # the inner loop will receive messages sent by
+                            # the client in buffers.  When a complete message
+                            # has been received, the loop will exit
+                            while True:
+                                data = reader.recv(data_chunk)
+                                print('received "{0}"'.format(data
+                                                              .decode('utf8')))
+                                if data:
+                                    reader.sendall(data)
+                                    print('sent "{0}"'.format(data
+                                                              .decode('utf8')))
+                                else:
+                                    print('message complete.\nclosing'
+                                          ' connection. {0}:{1}'.format(*addr))
+                                    inputs.remove(reader)
+                                    reader.close()
+                                    break
 
-                    if len(data) < data_chunk:
-                        break
-            except socket.error:
-                traceback.print_exc()
-                sys.exit(1)
-            finally:
-                conn.close()
-                print(
-                    'echo complete, client connection closed', file=log_buffer
-                )
+                        except socket.error as ex:
+                            if str(ex) == "[Errno 35] Resource temporarily" \
+                                          " unavailable":
+                                continue
+                            else:
+                                traceback.print_exc()
+                                sys.exit(1)
+
+            except select.error:
+                print("No further clients")
+                break
 
     except KeyboardInterrupt:
-        conn.close()
         print('quitting echo server', file=log_buffer)
         exit(1)
-    # pylint: enable=too-many-nested-blocks
+    finally:
+        for reader in inputs:
+            print('closing connection {}'.format(reader))
+            reader.close()
+
+        print('echo complete, client connections closed',
+              file=log_buffer)
+# pylint: enable=too-many-nested-blocks
+# pylint: enable=too-many-branches
+# pylint: enable=too-many-statements
+
 
 if __name__ == '__main__':
     server()
